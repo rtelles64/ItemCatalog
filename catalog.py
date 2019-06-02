41d2155f8fdcad3569e30c8ff38d7fc22bbee392
rtelles64 <rtelles64@gmail.com>
2019-06-02 01:42:24 -0700

    Add basic html code to test app

--- catalog.py
+++ catalog.py
@@ -6,13 +6,37 @@
 # sudo lsof -i :PORT
 # kill -9 PID
 
+# Set up Flask
 from flask import Flask
 app = Flask(__name__)
+
+# Import Database code
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Genre, Movie
+
+engine = create_engine('sqlite:///catalog.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 
 @app.route('/')
 def show_catalog():
-    return "I'm home!"
+    # To test, take out the first genre from our database
+    genre = session.query(Genre).first()
+    # List out all of the movies in that genre
+    movies = session.query(Movie).filter_by(genre_id=genre.id)
+
+    output = ''
+    # Test output to see we can retrieve info
+    for movie in movies:
+        output += movie.name
+        output += '</br>'
+        output += movie.description
+        output += '</br></br>'
+
+    return output
 
 
 if __name__ == '__main__':
